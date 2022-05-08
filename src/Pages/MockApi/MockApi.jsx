d9d7165bdbf988768c6848377403bcc352afe4ae
Mayur L Chawla <41817458+MayurChawla@users.noreply.github.com>
2022-05-08 17:38:53 +0530

    initial commit

---
+++ src/Pages/MockApi/MockApi.jsx
@@ -0,0 +1,11 @@
+import Mockman from "mockman-js";
+import "./mockApiStyle.css";
+
+const MockApi = () => {
+    return (
+    <div className="MockApi">
+        <Mockman />
+    </div>)
+}
+
+export default MockApi;
